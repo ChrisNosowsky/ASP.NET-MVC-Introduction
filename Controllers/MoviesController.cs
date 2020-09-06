a031d71d9f53b1afa7ef305927f26e5fcb51a8b7
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-05 22:51:21 -0400

    attribute based routing

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -35,7 +35,7 @@ namespace TestApplication.Controllers
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
-
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
